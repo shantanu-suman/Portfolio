b81680282efa805588c02f9aca680f4d66b832e7
Shantanu <2023si04016@wilp.bits-pilani.ac.in>
2025-07-23 21:45:55 +0530

    logo addition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -6,7 +5,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/home";
-import IntroAnimation from "@/components/IntroAnimation"; // Import animation
 
 function Router() {
   return (
@@ -18,18 +16,11 @@ function Router() {
 }
 
 function App() {
-  const [animationCompleted, setAnimationCompleted] = useState(false);
-
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <IntroAnimation onComplete={() => setAnimationCompleted(true)} />
-        {animationCompleted && (
-          <>
-            <Toaster />
-            <Router />
-          </>
-        )}
+        <Toaster />
+        <Router />
       </TooltipProvider>
     </QueryClientProvider>
   );
